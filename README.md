d84b8431887fb1749c1eb4c8a287fa84d19c5282
Yunbins <kig0632@naver.com>
2024-06-17 13:28:53 +0900

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# 1st_Mini_Project
--- sql/user11@edudb_view.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW member_task_view AS
-SELECT task_id, task_name, priority, completion, project_project_id, project_member_member_id, feedback_total_score, retry, memo
-FROM user11.task
-ORDER BY CASE priority 
-    WHEN 'High' THEN 1 
-    WHEN 'Medium' THEN 2 
-    WHEN 'Low' THEN 3 
-    ELSE 4 
-END;
